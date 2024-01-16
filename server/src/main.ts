b19ac2b4a3e8d847b672a327015ace18c4b82586
unknown <lfernando.r991@gmail.com>
2024-01-16 18:48:36 -0300

    add config validation pipe and cookie parse

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
+import * as cookieParser from 'cookie-parser'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.use(cookieParser())
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    })
+  )
+
+  // base routing
+  app.setGlobalPrefix('api_v1')
 
   await app.listen(process.env.APP_PORT)
 }
