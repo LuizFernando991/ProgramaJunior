58bd68e8c3e3aabf98d3af1cf125f6c68de91bec
unknown <lfernando.r991@gmail.com>
2024-01-16 18:41:23 -0300

    prisma setup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common'
 import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module'
 import { UsecasesProxyModule } from './infrastructure/usecases-proxy/usecase-proxy.module'
 import { ConfigModule } from '@nestjs/config'
+import { PrismaModule } from './infrastructure/db/prisma.module'
 
 @Module({
   imports: [
     EnvironmentConfigModule,
     UsecasesProxyModule.register(),
-    ConfigModule.forRoot({})
+    ConfigModule.forRoot({}),
+    PrismaModule
   ],
   controllers: [],
   providers: []
